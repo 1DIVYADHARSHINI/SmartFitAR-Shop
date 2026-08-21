d8e39f629a665b6b788a06497704bc6645299347
Divyadharshini K <162281554+1DIVYADHARSHINI@users.noreply.github.com>
2026-08-21 15:31:00 +0530

    Update API URL for seller endpoints

--- frontend/src/api/seller.api.js
+++ frontend/src/api/seller.api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 import API_URL from "@/config/api";
 
-{/*const API = "http://localhost:5000/api/sellers"; */}
-
 const API = `${API_URL}/api/sellers`;
-
 
 export const fetchSellers = () => axios.get(API);
 export const createSeller = (data) => axios.post(`${API}/create`, data);
