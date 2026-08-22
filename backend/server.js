3d992db61b0583f83dfc1bacac211af3b97e9dd1
Divyadharshini K <162281554+1DIVYADHARSHINI@users.noreply.github.com>
2026-08-22 11:20:58 +0530

    Update CORS configuration in server.js
    Restrict CORS to specific origin and enable credentials.

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://smartfitar.netlify.app", 
+  credentials: true,  
+  ));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
